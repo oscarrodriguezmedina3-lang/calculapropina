bd8419895ea33e6ecd896bc8701286ee9a767f0d
Oscar Rodriguez <oscarrodriguezmedina3@gmail.com>
2026-06-11 17:05:38 -0400

    rama_par_o_impar

---
+++ par_o_impar.js
@@ -0,0 +1,18 @@
+function parOImpar(numero) {
+if (numero % 2 === 0) {
+
+return `el numero ${numero} es par`;
+} else {
+return `el numero ${numero} es impar`;
+
+
+}
+
+
+}
+
+console.log(parOImpar(4));
+console.log(parOImpar(7));
+console.log(parOImpar(0));
+console.log(parOImpar(13));
+console.log(parOImpar(100));
